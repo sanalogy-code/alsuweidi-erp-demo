d135d5ecb2e9fb6eae4e1b546c78eeebaa527492
sanalogy-code <sana.diab@gmail.com>
2026-07-02 14:13:03 +0400

    Fix: remove extra closing div in EmployeeDetailModal
    Build: passes now.

--- frontend/src/components/hr/EmployeeDetailModal.jsx
+++ frontend/src/components/hr/EmployeeDetailModal.jsx
@@ -100,10 +100,8 @@ export default function EmployeeDetailModal({ employee, onClose }) {
             </div>
           </div>
         </div>
-      </div>
-
-        </div>
       )}
+
 
       {detailTab === 'visa' && (
         <div className="space-y-6">
